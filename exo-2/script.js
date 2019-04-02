a918e8368a66befbd37c42c45bd9f171290b2b3d
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-04-02 11:03:36 +0200

    Exo Tw0 V 0.1

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,33 @@
 // you can write js here
 console.log('exo-2');
+var isInscrit = false; // booléen
+var age = 25; // Integer
+
+//function inscrit(age, isInscrit) {
+
+    // on genere un nb aléatoire
+    var raceNumber = Math.floor(Math.random()*1000);        
+    /* conditions, test pour affichage */
+    /* si la V est égale à false=> n'est pas incrit */
+    if (isInscrit === false) {
+        // on affiche le message
+        console.log('Vous êtes inscrit sous le N° '+raceNumber);
+        //return(raceNumber);
+    } else {
+        // Sinon on affiche le message
+        console.log('Vous êtes déjà inscrit !');
+        //return(false);
+    }
+//}
+
+if(age>18 && isInscrit===true){
+    console.log('You will race at 9:30 am');
+} else if (isInscrit===true ^ age>18 ){
+    console.log('You will race at 11:00 am avec le N° '+raceNumber);
+} else if(isInscrit===false || age<18 ){
+    console.log('You will race at 12:30 am avec le N° '+raceNumber);
+} else {
+    console.log('No comprendo !!! ');
+}
+
+
