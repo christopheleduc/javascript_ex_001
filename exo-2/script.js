c47ee2549f3162149588007511fa91f163236857
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-04-02 09:41:36 +0200

    Exo One V 0.1

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,16 @@
 // you can write js here
 console.log('exo-2');
+//var kelvin = 294;
+var kelvin = prompt('Quelle est la température en Kelvin aujourd\'hui ?');
+// stock le résultat de la formule
+var celsius = kelvin - 273.15;
+// Affiche et concactènne String + Valeur...
+console.log('La temperature aujourd\'hui est de '+celsius+' degrés celcius.');
+// stock le résultat de la formule
+var fahrenheit = celsius * (9/5) + 32;
+// Affiche et concactènne String + Valeur...
+console.log('La temperature aujourd\'hui est de '+fahrenheit+' degrés fahrenheit.');
+// arrondi la valeur de fahrenheit à l'aide de la fonction floor
+var fahren_ar = Math.floor(fahrenheit);
+// Affiche et concactènne String + Valeur...
+console.log('La temperature aujourd\'hui est d\'environ '+fahren_ar+' degrés fahrenheit.');
